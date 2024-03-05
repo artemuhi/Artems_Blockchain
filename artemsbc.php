51136e393288d81095512bfd6642ff2acdf6574a
Artem <artem@noreply.localhost>
2024-03-05 09:50:08 +0300

    .

--- artemsbc.php
+++ artemsbc.php
@@ -1,10 +1,22 @@
 <?php
 class artembc {}
-function artembc_init() {}
+function artembc_init($filename, $lock=true, $max_size=4094) {
+    $bc=new artembc;
+    $bc->max_size=$max_size;
+    $bc->file=fopen($filename, "w+");
+    if ($lock) {
+        if (!flock($bc->file, LOCK_EX)) {
+            return false;
+            die("bcerror:locked_file");
+        }
+    }
+    
+}
 function artembc_createbc() {}
 function artembc_addtransaction() {}
 function artembc_initblock() {}
 function artembc_checkbc() {}
 function artembc_getblock() {}
-function artembc_getblocks() {}
+function artembc_getbc() {}
+function artembc_getnamebc() {}
 ?>
