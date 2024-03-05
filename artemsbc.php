a50b9598e9116c0ff2e4cd62f8f2997b32786c40
Artem <artem@noreply.localhost>
2024-03-05 08:47:50 +0300

    Скелет написан

---
+++ artemsbc.php
@@ -0,0 +1,10 @@
+<?php
+class artembc {}
+function artembc_init() {}
+function artembc_createbc() {}
+function artembc_addtransaction() {}
+function artembc_initblock() {}
+function artembc_checkbc() {}
+function artembc_getblock() {}
+function artembc_getblocks() {}
+?>
